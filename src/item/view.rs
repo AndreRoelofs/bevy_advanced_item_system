71bceb91fc8ad3a670114284b893876d319ffa8b
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2026-09-13 10:47:05 +0200

    feat(item): add observers for the other 2 item states

--- src/item/view.rs
+++ src/item/view.rs
@@ -1,12 +1,14 @@
 use bevy::{platform::collections::HashMap, prelude::*, scene::ScenePatch};
 
-use crate::{Item, ItemKey, OnGround, View, ViewOf};
+use crate::{EquippedBy, Item, ItemKey, OnGround, StoredIn, View, ViewOf};
 
 pub struct ItemViewPlugin;
 
 impl Plugin for ItemViewPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<ItemViewRegistry>()
+            .add_observer(change_view_on_equipped)
+            .add_observer(change_view_on_stored)
             .add_observer(change_view_on_ground);
     }
 }
@@ -38,17 +40,79 @@ fn change_view_on_ground(
     add: On<Add<OnGround>>,
     mut commands: Commands,
     views: Res<ItemViewRegistry>,
-    items: Query<(&Item, Option<&View>)>,
+    items: Query<&Item>,
 ) {
-    let Ok((item, view)) = items.get(add.entity) else {
+    let Ok(item) = items.get(add.entity) else {
         return;
     };
-    if view.and_then(View::entity).is_some() {
-        return;
-    }
+
+    commands.entity(add.entity).despawn_related::<View>();
+
     let Some(ground) = views
         .get(&item.key)
         .and_then(|definition| definition.chrome.get(OnGround::KEY))
+    else {
+        return;
+    };
+
+    commands.spawn((
+        ViewOf(add.entity),
+        // TODO: Make this point to a hand or something, for now this reads super
+        // weird. But it's better than nothing.
+        ChildOf(add.entity),
+        // Queues the spawning of the view for the next tick
+        ScenePatchInstance(ground.clone()),
+        Transform::default(),
+        Visibility::default(),
+    ));
+}
+
+fn change_view_on_equipped(
+    add: On<Add<EquippedBy>>,
+    mut commands: Commands,
+    views: Res<ItemViewRegistry>,
+    items: Query<&Item>,
+) {
+    let Ok(item) = items.get(add.entity) else {
+        return;
+    };
+
+    commands.entity(add.entity).despawn_related::<View>();
+
+    let Some(ground) = views
+        .get(&item.key)
+        .and_then(|definition| definition.chrome.get(EquippedBy::KEY))
+    else {
+        return;
+    };
+
+    commands.spawn((
+        ViewOf(add.entity),
+        // TODO: Make this point to a hand or something, for now this reads super
+        // weird. But it's better than nothing.
+        ChildOf(add.entity),
+        // Queues the spawning of the view for the next tick
+        ScenePatchInstance(ground.clone()),
+        Transform::default(),
+        Visibility::default(),
+    ));
+}
+
+fn change_view_on_stored(
+    add: On<Add<StoredIn>>,
+    mut commands: Commands,
+    views: Res<ItemViewRegistry>,
+    items: Query<&Item>,
+) {
+    let Ok(item) = items.get(add.entity) else {
+        return;
+    };
+
+    commands.entity(add.entity).despawn_related::<View>();
+
+    let Some(ground) = views
+        .get(&item.key)
+        .and_then(|definition| definition.chrome.get(StoredIn::KEY))
     else {
         return;
     };
